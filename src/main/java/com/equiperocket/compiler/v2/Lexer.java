3dcbe50811d15c065c2bb61452ef23bd277d2247
Diego-Pimenta <diegopimenta888@gmail.com>
2024-11-11 11:34:49 -0300

    fix: exception message

--- src/main/java/com/equiperocket/compiler/v2/Lexer.java
+++ src/main/java/com/equiperocket/compiler/v2/Lexer.java
@@ -60,7 +60,7 @@ public class Lexer {
 
             // Checagem para tokens nao validos
             if (!matched) {
-                throw new LexicalException("Unrecognized token at line", line, column);
+                throw new LexicalException("Unrecognized token", line, column);
             }
         }
         return tokens;
